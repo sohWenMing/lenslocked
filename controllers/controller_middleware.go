b069c3eae26ee109628f0d14de19e7f670697dde
nindgabeet <wenming.soh@gmail.com>
2025-10-16 02:56:39 +0800

    added trusted origin for prod

--- controllers/controller_middleware.go
+++ controllers/controller_middleware.go
@@ -60,6 +60,7 @@ func CSRFProtect(isDev bool, secretKey string) func(http.Handler) http.Handler {
 		csrf.Secure(isSetSecure),
 		csrf.TrustedOrigins([]string{
 			"localhost:3000",
+			"167.172.78.219:80",
 		},
 		),
 		csrf.Path("/"),
